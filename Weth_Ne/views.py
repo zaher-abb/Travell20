4dcf367c6020169bbd4c3d6f5bf79acf5d9137c2
Zaher Ab <52354101+zaher-abb@users.noreply.github.com>
2020-07-01 19:54:10 +0200

    Update views.py

--- Weth_Ne/views.py
+++ Weth_Ne/views.py
@@ -1,55 +1,55 @@
 from django.shortcuts import render
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 import requests
 import datetime
 from django.contrib import messages
+import os
 
 def weather(request):
-
- if request.method == 'POST':
-    city2=request.POST['weather']
-    context=""
-    url = f'https://api.openweathermap.org/data/2.5/weather?appid=40888a894484a9c947653e53e5c61f72&q={city2}'
-    url_checker=requests.get(url)
-    if url_checker.status_code==200:
+    if request.method == 'POST':
+    city2 = request.POST['weather']
+    context = ""
+    url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.envieroment.get(')}&q={city2}'
+    url_checker = requests.get(url)
+    if url_checker.status_code == 200:
         r = requests.get(url).json()
         city_weather = {
             'city': city2,
-            'temp':int( r['main']['temp'])-272,
+            'temp': int(r['main']['temp']) - 272,
             'des': r['weather'][0]['main'],
             'description': r['weather'][0]['description'],
             'icon': r['weather'][0]['icon'],
-            'min': int(r['main']['temp_min'])-272,
-            'max': int(r['main']['temp_max'])-272,
-            'feels': int(r['main']['feels_like'])-272,
+            'min': int(r['main']['temp_min']) - 272,
+            'max': int(r['main']['temp_max']) - 272,
+            'feels': int(r['main']['feels_like']) - 272,
             'wind': r['wind']['speed'],
             'pressure': r['main']['pressure'],
             'country': r['sys']['country'],
         }
         context = {'city_weather': city_weather}
         return render(request, 'weather_final.html', context)
-    else :
+    else:
         messages.info(request, 'city is not found')
         return redirect('weather:w')
 
- location_url='https://ipinfo.io/'
- location_data=requests.get(location_url).json()
- city2=location_data['city']
- url = f'https://api.openweathermap.org/data/2.5/weather?appid=40888a894484a9c947653e53e5c61f72&q={city2}'
- r = requests.get(url).json()
+    location_url = 'https://ipinfo.io/'
+    location_data = requests.get(location_url).json()
+    city2 = location_data['city']
+    url = f'https://api.openweathermap.org/data/2.5/weather?appid=40888a894484a9c947653e53e5c61f72&q={city2}'
+    r = requests.get(url).json()
 
- city_weather = {
-     'city': city2,
-     'temp': int(r['main']['temp']) - 272,
-     'des': r['weather'][0]['main'],
-     'description': r['weather'][0]['description'],
-     'icon': r['weather'][0]['icon'],
-     'min': int(r['main']['temp_min']) - 272,
-     'max': int(r['main']['temp_max']) - 272,
-     'feels': int(r['main']['feels_like']) - 272,
-     'wind': r['wind']['speed'],
-     'pressure': r['main']['pressure'],
-     'country': r['sys']['country'],
- }
- context = {'city_weather': city_weather}
- return render(request, 'weather_final.html',context)
+    city_weather = {
+        'city': city2,
+        'temp': int(r['main']['temp']) - 272,
+        'des': r['weather'][0]['main'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'],
+        'min': int(r['main']['temp_min']) - 272,
+        'max': int(r['main']['temp_max']) - 272,
+        'feels': int(r['main']['feels_like']) - 272,
+        'wind': r['wind']['speed'],
+        'pressure': r['main']['pressure'],
+        'country': r['sys']['country'],
+    }
+    context = {'city_weather': city_weather}
+    return render(request, 'weather_final.html', context)
